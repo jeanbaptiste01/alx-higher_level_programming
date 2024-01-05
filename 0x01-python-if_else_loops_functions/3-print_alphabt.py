82e97ee0ef49ad6ed101e861dba8df4bd4c92d33
jeanbabtiste01 <muhayejean7@gmail.com>
2024-01-04 16:06:20 -0800

    3-print_alphabt

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) not in ('q', 'e'):
+        print("{:s}".format(chr(i)), end="")
+
+print()  # To print a new line at the end (optional)
